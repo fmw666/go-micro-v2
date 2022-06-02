f8d34cc862809ca570c91a9c31454bef05915005
fmw666 <fmw19990718@qq.com>
2022-06-02 14:28:34 +0800

    order 微服务代码调整

--- code/order/models/init.go
+++ code/order/models/init.go
@@ -18,3 +18,10 @@ func Init() *gorm.DB {
 	}
 	return db
 }
+
+func Migrate() {
+	err := DB.Set(`gorm:table_options`, "charset=utf8mb4").AutoMigrate(&Order{})
+	if err != nil {
+		log.Println("gorm Init Error : ", err)
+	}
+}
