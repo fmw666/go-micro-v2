f2aaf5a5791d68ce092bba70c511086d6a6f081e
fmw666 <fmw19990718@qq.com>
2023-03-31 16:16:56 +0800

    管理文件修改

--- code/manage.go
+++ code/manage.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"reflect"
 	"strings"
 )
@@ -42,14 +43,18 @@ func (s *Script) Init() {
 	dirs, _ := ioutil.ReadDir(".")
 	for _, dir := range dirs {
 		if dir.IsDir() {
+			fmt.Println("开始初始化 " + dir.Name() + " 服务...")
 			// 切换当前工作目录
 			os.Chdir(dir.Name())
-			if dir.Name() == "api-gateway" {
-				// 初始化 swagger 文件
-				exec.Command("swag", "init").Run()
+			// 初始化 swagger 文件
+			err := runCmd("swag init")
+			if err != nil {
+				err = runCmd("go install github.com/swaggo/swag/cmd/swag@latest")
+				runCmd("swag init")
 			}
 			// 初始化 go mod
-			exec.Command("go", "mod", "tidy").Run()
+			runCmd("go mod tidy")
+			fmt.Println(OK + " " + GreenBG + dir.Name() + " 服务初始化完成" + Font)
 		}
 	}
 	os.Chdir("..")
@@ -63,15 +68,28 @@ func (s *Script) Start() {
 	for _, dir := range dirs {
 		if dir.IsDir() {
 			fmt.Println("开始启动 " + dir.Name() + " ...")
-			cmd := "cd " + pwdDir + "/" + dir.Name() + " && go run main.go\n"
+
+			serviceDir := filepath.Join(pwdDir, dir.Name())
+			mainFile := filepath.Join(serviceDir, "main.go")
+			// 判断 main.go 文件是否存在
+			if _, err := os.Stat(mainFile); os.IsNotExist(err) {
+				fmt.Println(Error + " " + RedBG + dir.Name() + " 服务不存在 main.go 文件" + Font)
+				continue
+			}
+
+			cmdArgs := []string{"cd", serviceDir, "&&", "go", "run", "main.go"}
 			sessionName := dir.Name() + "-service"
 
-			runCmd(fmt.Sprint("screen -S " + sessionName + " -X quit"))
-			runCmd(fmt.Sprint("screen -dmS " + sessionName))
-			runCmd(fmt.Sprint("screen -S " + sessionName + " -p 0 -X stuff " + cmd))
+			runCmd(fmt.Sprintf("screen -S %s -X quit", sessionName))
+			runCmd(fmt.Sprintf("screen -dmS %s", sessionName))
+			err := runCmd(fmt.Sprintf("screen -S %s -p 0 -X stuff \"%s\n\"", sessionName, strings.Join(cmdArgs, " ")))
+			if err != nil {
+				fmt.Println(Error + " " + RedBG + dir.Name() + " 启动失败" + Font)
+			} else {
+				fmt.Println(OK + " " + GreenBG + dir.Name() + " 启动完成" + Font)
+			}
 		}
 	}
-	fmt.Println(OK + " " + GreenBG + "service start !" + Font)
 }
 
 // 关闭所有微服务及网关
@@ -82,9 +100,9 @@ func (s *Script) Stop() {
 			fmt.Println("开始关闭 " + dir.Name() + " ...")
 			sessionName := dir.Name() + "-service"
 			runCmd(fmt.Sprint("screen -S " + sessionName + " -X quit"))
+			fmt.Println(OK + " " + GreenBG + dir.Name() + " 关闭完成" + Font)
 		}
 	}
-	fmt.Println(OK + " " + GreenBG + "service stop !" + Font)
 }
 
 // 展示所有的 session
@@ -93,31 +111,32 @@ func (s *Script) Ls() {
 }
 
 // 调用 Linux 命令
-func runCmd(cmdStr string) {
+func runCmd(cmdStr string) error {
 	//需要执行的命令
 	cmd := exec.Command("/bin/bash", "-c", cmdStr)
 	// 获取管道输入
 	output, err := cmd.StdoutPipe()
 	if err != nil {
 		fmt.Println("无法获取命令的标准输出管道", err.Error())
-		return
+		return err
 	}
 	// 执行Linux命令
 	if err := cmd.Start(); err != nil {
 		fmt.Println("Linux命令执行失败，请检查命令输入是否有误", err.Error())
-		return
+		return err
 	}
 	// 读取所有输出
 	bytes, err := ioutil.ReadAll(output)
 	if err != nil {
 		fmt.Println("打印异常，请检查")
-		return
+		return err
 	}
 	if err := cmd.Wait(); err != nil {
 		// fmt.Println("Wait", err.Error())
-		return
+		return err
 	}
 	fmt.Printf("%s", bytes)
+	return nil
 }
 
 // 检查是否有 screen 命令
